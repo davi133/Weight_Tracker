29b89e914ceaa2b3ce2c23c7b692dd83b542a8dd
davi133 <dqavi13@gmail.com>
2022-08-24 03:42:01 -0300

    progresso com perfis e criação de contas

--- src/HomePage/NewProfileForm.js
+++ src/HomePage/NewProfileForm.js
@@ -1,22 +1,77 @@
-
-
+import { useState } from "react";
+import Profile, { saveProfile, retrieveAccount } from "../model/Profiles";
 
 export default function NewProfileForm(props) {
 
+    const [inputs, setInputs] = useState({});
+
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setInputs(values => ({ ...values, [name]: value }))
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        
+        if (inputs.senha !== inputs.senha2)
+        {
+            alert("confirme sua senha");
+        }
+        else if (retrieveAccount(inputs.email))
+        {
+            alert("já existe uma conta com esse email");
+        }
+        else
+        {
+            saveProfile(new Profile(inputs.name,inputs.email,inputs.senha));
+            alert("conta criada");
+        }
 
 
-
+    }
 
 
 
     return (
         <div className="NewProfileComponent" style={props.style} onClick={props.onClick}>
-            
-            <div className="screenBlocker" onClick={()=>{ props.onCancel();}}>
+            <div className="screenBlocker" onClick={() => { props.onCancel(); setInputs({}) }}>
+
             </div>
 
             <div className="newProfileFormBody" onClick={props.onFormClick}>
-                
+                <h2>Criar novo Perfil</h2>
+
+                <form className="the_form" onSubmit={handleSubmit}>
+                    <label>Name:
+                        <br></br>
+                        <input type="text" value={inputs.name|| ""} name="name" onChange={handleChange} required/>
+                    </label>
+
+                    <label>Email:
+                        <br></br>
+                        <input type="email" value={inputs.email|| ""} name="email" onChange={handleChange} required/>
+                    </label>
+
+                    <label>Senha:
+                        <br></br>
+                        <input type="password" value={inputs.senha|| ""} name="senha" onChange={handleChange} required/>
+                    </label>
+                    <label>Confirme a senha:
+                        <br></br>
+                        <input type="password" value={inputs.senha2|| ""} name="senha2" onChange={handleChange} required/>
+                    </label>
+
+
+                    <div>
+                    <input type="submit" value="Cadastrar"/>
+                    <input  type="button" value="Fechar" onClick={() => { props.onCancel(); setInputs({}) }} />
+                    </div>
+
+                </form>
+               
+
+
             </div>
         </div>
     );
---
+++ src/model/Profiles.js
@@ -0,0 +1,42 @@
+export default function Profile(name, email="sem email", senha="12345")
+{
+    var newProfile=
+    {
+       name:name,
+       email:email,
+       senha:senha
+
+    };
+    return newProfile;
+}
+
+
+export function retrieveAllAccounts()
+{
+
+    var lista = localStorage.getItem("WT_profiles") ||"{}";
+    return JSON.parse(lista);
+    
+}
+
+export function retrieveAccount(email)
+{
+    var lista = retrieveAllAccounts();
+    return lista[email];
+
+}
+
+export function saveProfile(profile)
+{
+    var lista = retrieveAllAccounts();
+    lista = {...lista, [profile.email]: profile}
+    localStorage.setItem("WT_profiles",JSON.stringify(lista))
+}
+
+export function removeAllAccounts()
+{
+    localStorage.removeItem("WT_profiles");
+    console.log("contas apagadas");
+}
+
+window.removeAllAccounts = removeAllAccounts;
